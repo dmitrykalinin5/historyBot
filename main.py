701e2841fc0563c58500ab5ef61a3c7a1004620f
dmitry <dkalinin240@gmail.com>
2025-05-13 13:01:44 +0300

    kalinin

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import telebot
+from telebot import types
+from telebot.types import InputMediaPhoto
 
 token = "7875328646:AAEnQegv0NAX3qnEs01m-Qbxznnn7zijH8k"
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(command=["start"])
 def start(message):
+    with open("")
 
 
 bot.polling()
