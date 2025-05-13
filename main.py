6116fe026c5a4ae565f15835c35ccf06567246f4
dmitry <dkalinin240@gmail.com>
2025-05-13 13:07:59 +0300

    kalinin

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import telebot
 from telebot import types
 from telebot.types import InputMediaPhoto
 
-token = "7875328646:AAEnQegv0NAX3qnEs01m-Qbxznnn7zijH8k"
+token = "8178537704:AAEQrPjGPzx6dVjgchrSYlf4ksRDvg5iW0o"
 bot = telebot.TeleBot(token)
+bot.remove_webhook()
 
 @bot.message_handler(command=["start"])
 def start(message):
-    with open("")
+    with open("startphoto.jpg") as f1:
+        photo = InputMediaPhoto(f1, caption="aaaa")
+        bot.send_media_group(message.chat.id, [photo])
 
 
 bot.polling()
