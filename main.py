f93463c193f557f3aa55b2931556d5c61d8ea0a1
dmitry <dkalinin240@gmail.com>
2025-05-13 12:58:58 +0300

    kalinin

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import telebot
 token = "7875328646:AAEnQegv0NAX3qnEs01m-Qbxznnn7zijH8k"
 bot = telebot.TeleBot(token)
 
+@bot.message_handler(command=["start"])
+def start(message):
+
+
 bot.polling()
